866f92d12230f8857a1992a8186cd53a4eb8f726
cncoderalex <cncoderalex@yahoo.com>
2019-04-26 09:41:21 +0800

    Update qsort.cpp

--- qsort.cpp
+++ qsort.cpp
@@ -1,6 +1,67 @@
-#include<stdio.h>
+#include <iostream>
+using namespace std;
+
+int Partion(int Ary[], int s, int e)
+{
+	if (!Ary || s >= e)
+	{
+		return s;
+	}
+
+	int s1 = s;
+	int e1 = e;
+	int temp = Ary[s1];
+
+	while (s1 < e1)
+	{
+		while (s1 < e1 && Ary[e1] >= temp)
+		{
+			e1--;
+		}
+		Ary[s1] = Ary[e1];
+
+		while (s1 < e1 && Ary[s1] <= temp)
+		{
+			s1++;
+		}
+		Ary[e1] = Ary[s1];
+	}
+
+	Ary[s1] = temp;
+
+	return s1;
+}
+
+void QSort(int Ary[], int s, int e)
+{
+	if (!Ary || s >= e)
+	{
+		return;
+	}
+
+	int mid = Partion(Ary, s, e);
+	QSort(Ary, s, mid-1);
+	QSort(Ary, mid + 1, e);
+}
+
+void Print(int Ary[], int count)
+{
+	for (int i = 0; i < count; i++)
+	{
+		printf("%d ",  Ary[i]);
+	}
+
+	printf("\n");
+}
 
 int main()
 {
-    return 0;
+	int Ary[] = {8, 10, 5, 4, 3, 12, 10, 9, 4, 56, 43, 78, 27};
+	int count = sizeof(Ary) / sizeof(int);
+	QSort(Ary, 0, count-1);
+	Print(Ary, count);
+
+	system("pause");
+
+	return 0;
 }
